dfa4ed19cb4720b2a6dd18da8465003fc6451226
Vitalii Sosluik <soslukven@gmail.com>
2021-03-27 15:03:01 +0200

    Create Form and submit it, and validate form

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
+    <Form @submitForm="onFormSubmit" />
     <TotalBalance :total="totalBalance" />
-    <BudgetList :list="list" @deleteItem="onDeleteItem"/>
+    <BudgetList :list="list" @deleteItem="onDeleteItem" />
   </div>
 </template>
 
 <script>
 import BudgetList from './components/BudgetList';
 import TotalBalance from './components/TotalBalance';
+import Form from './components/Form';
 
 export default {
   name: 'App',
   components: {
     BudgetList,
     TotalBalance,
+    Form,
   },
   data: () => ({
     list: {
@@ -32,14 +35,25 @@ export default {
     },
   }),
   computed: {
-      totalBalance(){
-          return Object.values(this.list).reduce((acc, item)=> acc + item.value, 0);
-      }
+    totalBalance() {
+      return Object.values(this.list).reduce(
+        (acc, item) => acc + item.value,
+        0
+      );
+    },
   },
   methods: {
     onDeleteItem(id) {
       this.$delete(this.list, id);
-    }
+    },
+    onFormSubmit(data) {
+      const newObj = {
+        ...data,
+        id: String(Math.random()),
+      };
+
+      this.$set(this.list, newObj.id, newObj);
+    },
   },
 };
 </script>
